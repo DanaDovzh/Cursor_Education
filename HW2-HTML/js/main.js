61d3c362d50a5cf1a4e966a173a0c531a6f9b6bd
DanaDovzh <danadovzh16@gmail.com>
2020-09-08 18:27:40 +0300

    Fixes with tabs

--- HW2-HTML/js/main.js
+++ HW2-HTML/js/main.js
@@ -1,4 +1,4 @@
-function unDo(){
+function unDo() {
     document.getElementById('finish').innerHTML = 'Як тільки довчу js,' + '<br/>' +'ти зразу будеш в таблиці героїв!';
     document.getElementById('mb').innerHTML = '(можливо :)';
-	}
+}
