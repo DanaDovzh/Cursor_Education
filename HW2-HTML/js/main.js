a4f0f4371239f819dde46d0e553c6d635295e004
DanaDovzh <danadovzh16@gmail.com>
2020-09-08 17:33:44 +0300

    Add HW2 with .html, .css, .js, img

---
+++ HW2-HTML/js/main.js
@@ -0,0 +1,4 @@
+function unDo(){
+    document.getElementById('finish').innerHTML = 'Як тільки довчу js'+'\n\n' +'ти зразу будеш в таблиці героїв!';
+    document.getElementById('mb').innerHTML = '(можливо :)';
+	}
