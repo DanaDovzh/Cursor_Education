5d208542f8194e4f2f80dd46146eefe099a107da
DanaDovzh <danadovzh16@gmail.com>
2020-09-24 22:50:01 +0300

    added functions for number-text

--- HW7-Pseudo-classes/js/main.js
+++ HW7-Pseudo-classes/js/main.js
@@ -2,7 +2,10 @@
         let n = 1 + Math.floor(Math.random()*10);
         document.getElementById(NameId).style.backgroundImage = "url(img/"+n+".jpg)" ;
         document.getElementById(NameId).style.backgroundSize = "cover";
+        document.getElementById(NameId).innerHTML = '';
+
     }
     function back(NameId) {
         document.getElementById(NameId).style.background = "rgb(92, 90, 90)";
+        document.getElementById(NameId).innerHTML = NameId;
     }
