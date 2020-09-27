37106af83261bdf431bcb42099a42338ac1207b1
DanaDovzh <danadovzh16@gmail.com>
2020-09-27 16:46:17 +0300

    Fix link for img

--- HW7-Pseudo-classes/js/main.js
+++ HW7-Pseudo-classes/js/main.js
@@ -1,6 +1,6 @@
 	function myFunc (NameId) {
         let n = 1 + Math.floor(Math.random()*10);
-        document.getElementById(NameId).style.backgroundImage = "url(img/"+n+".jpg)" ;
+        document.getElementById(NameId).style.backgroundImage = `url(img/${n}.jpg)` ;
         document.getElementById(NameId).style.backgroundSize = "cover";
         document.getElementById(NameId).innerHTML = '';
 
