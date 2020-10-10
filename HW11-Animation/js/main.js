5b9040bfd602cfbe70112708d4198163a44ce82f
DanaDovzh <danadovzh16@gmail.com>
2020-10-10 17:18:47 +0300

    Add second animation and script for it

---
+++ HW11-Animation/js/main.js
@@ -0,0 +1,5 @@
+function swords() {
+    document.getElementById("FirstSword").classList.toggle("sword-animation-first")
+    document.getElementById("SecondSword").classList.toggle("sword-animation-second")
+    document.getElementById("ThirdSword").classList.toggle("sword-animation-third")
+}
