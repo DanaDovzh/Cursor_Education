461fc4b747e126ba110382ba3326c9c615730ecb
DanaDovzh <danadovzh16@gmail.com>
2020-10-18 14:20:31 +0300

    Rewrite function

--- HW13-BEM/js/main.js
+++ HW13-BEM/js/main.js
@@ -4,7 +4,7 @@ var currentActive;
 for (var i = 0; i < tabs.length; i++) {
   tabs[i].addEventListener("click", function() {
     currentActive = document.getElementsByClassName("active");
-    currentActive[0].className = currentActive[0].className.replace(" active", "");
-    this.className += " active";
+    currentActive[0].classList.remove("active");
+    this.classList.add("active");
   });
 }
