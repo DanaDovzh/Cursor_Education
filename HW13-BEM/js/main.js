ab432c9b35e2779a329383a4b30f19d6570d012a
DanaDovzh <danadovzh16@gmail.com>
2020-10-20 21:35:58 +0300

    Add fixes

--- HW13-BEM/js/main.js
+++ HW13-BEM/js/main.js
@@ -1,9 +1,12 @@
 
-var tabs = document.getElementsByClassName("tab-sml");
+// var tabs = document.getElementsByClassName("tab-sml");
 var currentActive;
+let header = document.getElementById("first");
+var tabs = header.getElementsByClassName("tab-sml");
+
 for (var i = 0; i < tabs.length; i++) {
   tabs[i].addEventListener("click", function() {
-    currentActive = document.getElementsByClassName("active");
+    currentActive = header.getElementsByClassName("active");
     currentActive[0].classList.remove("active");
     this.classList.add("active");
   });
--- HW14-Optimization-of-the-final-project/js/main.js
+++ HW14-Optimization-of-the-final-project/js/main.js
@@ -1,4 +1,4 @@
-
+'use strict';
 $(document).ready(function () {
     $('.single-item').slick({
  
