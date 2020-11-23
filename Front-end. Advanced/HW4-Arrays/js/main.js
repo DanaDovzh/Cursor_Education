54663e04f754f2f98dee705b6ca8eba7a0fe4cf3
DanaDovzh <danadovzh16@gmail.com>
2020-11-23 14:09:25 +0200

    Fix some mistakes

--- Front-end. Advanced/HW4-Arrays/js/main.js
+++ Front-end. Advanced/HW4-Arrays/js/main.js
@@ -4,9 +4,20 @@ const marks = [4, 5, 5, 3, 4, 5];
 const femaleNames = ["Олена", "Іра", "Світлана"];
 const maleNames = ["Олександр", "Ігор", "Олексій"];
 
-const pairing = (names) => {
+const placeTeamStudent = document.querySelectorAll(".team-students");
+const placeTeamTheme = document.querySelectorAll(".theme-for-team");
+const placeMarkTeam = document.querySelectorAll(".mark-for-team");
+const placeNameStudent = document.querySelectorAll(".student");
+const placeMarkStudent = document.querySelectorAll(".mark-for-student");
+const styleForLog = ['padding: 3px;',
+    'border-radius: 5px;',
+    'background: linear-gradient(101deg, rgba(39,85,4,0.5340511204481793) 0%, rgba(35,157,22,0.5088410364145659) 100%);',
+    'color: black;'].join('');
+
+const createStudentPairs = (names) => {
     let studentsPairs = [];
-    for (let i = 0; i < Math.ceil((students.length)/2); i++) studentsPairs[i] = [];
+    const numberOfPairs = Math.ceil((students.length)/2);
+    for (let i = 0; i < numberOfPairs; i++) studentsPairs[i] = [];
     let indexNewStudent = 0;
         divideIntoGenders(maleNames, 0);
         indexNewStudent = 0;
@@ -23,9 +34,10 @@ const pairing = (names) => {
         return studentsPairs;
 };
 
-const themesForTeamsStudents = (pairsStudents) => {
+const themesForTeamsStudents = (pairsStudents, themesForTeam) => {
     const pairsStudentsWithThemes = [];
-    for (let i = 0; i < themes.length; i++) pairsStudentsWithThemes.push([pairsStudents[i].join("\u00A0і\u00A0"),themes[i]]);
+    for (let i = 0; i < themes.length; i++)     
+        pairsStudentsWithThemes.push([pairsStudents[i].join("\u00A0і\u00A0"),themesForTeam[i]]);
     return pairsStudentsWithThemes;
 };
 
@@ -34,28 +46,21 @@ const getRandomNumber = function (min = 1, max = 5) {
 }
 
 const marksTeamsStudents = (marksProject) => {
-    const marksAndTeams = [...marksProject];
-    for (let i = 0; i < marksAndTeams.length; i++) {
-        marksAndTeams[i].push(getRandomNumber(1,5));
-    };
+    const marksAndTeams = JSON.parse(JSON.stringify(marksProject));
+    for (let i = 0; i < marksAndTeams.length; i++)  marksAndTeams[i].push(getRandomNumber(1,5));
     return marksAndTeams;
 };
 
-const markOneStudent = (students) => {
+const markOneStudent = (students, markToStudent) => {
     const markStudent = [];
-    for (let i = 0; i < students.length; i++) markStudent.push([students[i], marks[i]]);
+    for (let i = 0; i < students.length; i++) markStudent.push([students[i], markToStudent[i]]);
     return markStudent;
 };
 
-const teamsStudents = pairing(students);
-const themesAndStudents = themesForTeamsStudents(teamsStudents);
-const marksForTeams = marksTeamsStudents(themesForTeamsStudents(teamsStudents));
-const markForStudent = markOneStudent(students);
-const placeTeamStudent = document.querySelectorAll(".team-students");
-const placeTeamTheme = document.querySelectorAll(".theme-for-team");
-const placeMarkTeam = document.querySelectorAll(".mark-for-team");
-const placeNameStudent = document.querySelectorAll(".student");
-const placeMarkStudent = document.querySelectorAll(".mark-for-student");
+const teamsStudents = createStudentPairs (students);
+const themesAndStudents = themesForTeamsStudents(teamsStudents, themes);
+const marksForTeams = marksTeamsStudents(themesAndStudents);
+const markForStudent = markOneStudent(students, marks);
 
 const makePairStudentsForLog = (arrayForTransform) => {
     const arrayTransformed = [];
@@ -75,6 +80,7 @@ const makePairsThemesForLog = (arrayForTransform) => {
     }
     return arrayTransformed;
 };
+
 const makePairsThemesMarksForLog = (arrayForTransform) => {
     const arrayTransformed = [];
     for (let i = 0; i < arrayForTransform.length; i ++) {
@@ -100,13 +106,8 @@ const consolResult = (tablePrint, arrayPrint, messagePrint) => {
     console.group('%c%s', styleForLog, messagePrint);
     console.table(tablePrint);
     console.log(arrayPrint);
-console.groupEnd();
+    console.groupEnd();
 };
-
-let styleForLog = ['padding: 3px;',
-'border-radius: 5px;',
-  'background: linear-gradient(101deg, rgba(39,85,4,0.5340511204481793) 0%, rgba(35,157,22,0.5088410364145659) 100%);',
-  'color: black;'].join('');
 
 consolResult(makePairStudentsForLog(teamsStudents), teamsStudents, "Пари студентів");
 consolResult(makePairsThemesForLog(themesAndStudents), themesAndStudents, "Теми проектів для пар студентів");
