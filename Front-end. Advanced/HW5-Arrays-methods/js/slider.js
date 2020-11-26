6b26a59a2f343597cd6d9684b608b4cd75e9ebca
DanaDovzh <danadovzh16@gmail.com>
2020-11-26 16:32:08 +0200

    Add HW5

---
+++ Front-end. Advanced/HW5-Arrays-methods/js/slider.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function(){
+    $('.slider-functions').slick({
+        dots: true,
+        dotsClass: "slider__dots",
+        slidesToShow: 1,
+        prevArrow: false,
+        nextArrow: false,
+    });
+});
