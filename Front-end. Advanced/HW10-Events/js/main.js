866c9b5d0e296fae7ba4fdf8e8db5ed70cf42e6b
DanaDovzh <danadovzh16@gmail.com>
2020-12-17 23:22:32 +0200

    fix

--- Front-end. Advanced/HW10-Events/js/main.js
+++ Front-end. Advanced/HW10-Events/js/main.js
@@ -13,34 +13,30 @@ window.setTimeout(function () {
 }, TIME_LOADED);
 
 const randomNumber = (min = 1, max = 11) => Math.floor(Math.random() * (max - min)) + min;
+
+const styleActiveSound = (numberSound) => {
+  textNotLetter.style.display = "none";
+  document.querySelector(`#${numberSound}`).style.cssText = "box-shadow: 60px 20px 40px 20px #428305; background-color: #9c9b9b;";
+}
 const playSound = (nameSound) => {
   audio.src = `sounds/${nameSound}.mp3`;
-  textNotLetter.style.display = "none";
   audio.play();
   audio.volume = 0.6;
-  document.querySelector(`#${nameSound}`).style.cssText = "box-shadow: 60px 20px 40px 20px #428305; background-color: #9c9b9b;";
 }
 
 document.addEventListener('keydown', (event) => {
   const keyName = event.code;
   const enterLetter = keyName[3];
-  if (keyName.startsWith(KEY_CODE)) {
-    if (soundLetter.includes(enterLetter))
-      playSound(enterLetter)
-    else {
-      audio.pause();
-      textNotLetter.style.display = "block";
-    }
-  }
+  (keyName.startsWith(KEY_CODE)) && (soundLetter.includes(enterLetter) ?  
+    (playSound(enterLetter), styleActiveSound(enterLetter)) : (audio.pause(),
+                            textNotLetter.style.display = "block"));
 }, false);
 
 snowmans.forEach((snowman, index) => {
   snowmans[index].addEventListener('click', () => {
-    (snowmans[index].closest(`.${soundLetter[index]}`)) ? playSound(soundLetter[index]) : textNotLetter.style.display = "block";
+    (snowmans[index].closest(`.${soundLetter[index]}`)) ? (playSound(soundLetter[index]), styleActiveSound(soundLetter[index])) : textNotLetter.style.display = "block";
   })
-})
 
-snowmans.forEach((snowman, index) => {
   snowmans[index].addEventListener('mouseenter', () => {
     snowmans[index].querySelector(`#${soundLetter[index]}`).src = `img/change_${randomNumber()}.svg`;
   })
