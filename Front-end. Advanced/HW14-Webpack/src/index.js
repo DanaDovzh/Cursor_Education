58af195e18d7ca0cadeb5500ae70cfe9d930d6cf
DanaDovzh <danadovzh16@gmail.com>
2020-12-28 14:51:10 +0200

    Correct

--- Front-end. Advanced/HW14-Webpack/src/index.js
+++ Front-end. Advanced/HW14-Webpack/src/index.js
@@ -1,10 +1,10 @@
 import './css/style.css';
 import "regenerator-runtime/runtime";
-import {isPalyndrom, getRandomNumber} from './js/HWs/hw_3';
-import {generateRandomColor} from './js/HWs/hw_9';
-import {generateCombinations} from './js/HWs/hw_5';
-import {makeCharacter, makeChineseWord} from './js/HWs/hw_11';
-import {playSound} from './js/HWs/hw_10';
+import { isPalyndrom, getRandomNumber } from './js/HWs/hw_3';
+import { generateRandomColor } from './js/HWs/hw_9';
+import { generateCombinations } from './js/HWs/hw_5';
+import { makeCharacter, makeChineseWord } from './js/HWs/hw_11';
+import { playSound } from './js/HWs/hw_10';
 import O from './audio/O.mp3';
 import S from './audio/S.mp3';
 import B from './audio/B.mp3';
@@ -20,7 +20,8 @@ const answerCombinations = document.querySelector("#answer-combinaions");
 const wordCombinations = document.querySelector("#word-combinations");
 const btnMakeChinese = document.querySelector("#chinese");
 const answerChinese = document.querySelector("#answer-chinese");
-
+const audio = new Audio();
+const letterBlocks = document.querySelectorAll(".letter-sound");
 const textForPalindron = () => isPalyndrom(wordPalindrom.value) ? "is" : "is't";
 formPalindrom.addEventListener("submit", (event) => {
     answerPalindrom.innerHTML = `This word ${textForPalindron()} palindrom!`;
@@ -37,39 +38,45 @@ btnChangeBg.addEventListener("click", (e) => {
     mainBlock.style.backgroundImage = `radial-gradient( circle farthest-corner at 10% 20%,  ${generateRandomColor()} 1.1%, ${generateRandomColor()} 43.7%, ${generateRandomColor()} 89.7% )`;
 });
 
-btnMakeChinese.addEventListener("click", async ()=> {
+btnMakeChinese.addEventListener("click", async () => {
     answerChinese.innerHTML = await makeCharacter(getRandomNumber(1, 10));
 });
-const audio = new Audio();
+
 document.addEventListener('keydown', (event) => {
     const keyName = event.code;
     const enterLetter = keyName[3];
+    letterBlocks.forEach(element => {
+        element.style.color = "#fff";
+    })
+    console.log(letterBlocks);
     if (keyName.startsWith("Key")) {
-    switch (enterLetter) {
-        case "O":
-            audio.pause();
-            playSound(audio, O);
-            break;
-        case "S":
-            audio.pause();
-            playSound(audio, S);
-            break;
-        case "B":
-            audio.pause();
-            playSound(audio, B);
-            break;
-        case "N":
-            playSound(audio, N);
-            break;
-        case "H":
-            playSound(audio, H);
-            break;
-        default:
-            playSound(audio, other);
-        
+        const activeLetter = document.querySelector(`#${enterLetter}`)
+        activeLetter.style.color = "#1e7e34";
+        switch (enterLetter) {
+            case "O":
+                audio.pause();
+                playSound(audio, O);
+                break;
+            case "S":
+                audio.pause();
+                playSound(audio, S);
+                break;
+            case "B":
+                audio.pause();
+                playSound(audio, B);
+                break;
+            case "N":
+                playSound(audio, N);
+                break;
+            case "H":
+                playSound(audio, H);
+                break;
+            default:
+                playSound(audio, other);
+
+        };
     };
-};
-  }, false);
+}, false);
 
 
 
